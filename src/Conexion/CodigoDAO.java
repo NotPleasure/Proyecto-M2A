5da6798f1dad365bfd18b96f9aa18215d72285d8
USER <USER@192.168.1.26>
2025-07-14 16:30:03 -0500

    Prueba2

--- src/Conexion/CodigoDAO.java
+++ src/Conexion/CodigoDAO.java
@@ -19,6 +19,8 @@ public class CodigoDAO {
         try (Connection conn = ConexionPostgres.conectar();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             
+            
+            
             ps.setString(1, email);
             ps.setString(2, codigo);
             
